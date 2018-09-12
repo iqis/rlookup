54e0fc6f28fe7160693e815644de24513bdace07
Iqis Gnahz <iqis.gnahz@gmail.com>
2018-09-11 23:12:50 -0400

    changed name to "rlookup", functions, vignettes

--- R/core.R
+++ R/core.R
@@ -1,8 +1,8 @@
-#' jollyrogr
+#' rlookup
 #'
 #' Recoding variables in a data frame through a value map
 #'
-#' @name jollyrogr-package
+#' @name rlookup-package
 #' @docType package
 "_PACKAGE"
 
@@ -14,48 +14,48 @@
 #' @param ... column names, tidy
 #' @import dplyr purrr
 #' @export
-cover <- function(.data, ...){
+make_lookup <- function(.data, ...){
         dots <- quos(...)
         unique_values <-   .data %>%
                 select(!!!dots) %>%
                 map(unique)
         namez <- names(unique_values) # colnames
 
-        cover <- data.frame()
+        lookup <- data.frame()
         for (i in seq_along(unique_values)) {
                 single <- data.frame(namez[i], unique_values[i], NA)
                 names(single) <- c("col_name", "old_value", "new_value")
-                cover <- rbind(cover, single)
+                lookup <- rbind(lookup, single)
         }
 
-        return(cover %>% transmute_all(as.character) %>% as_tibble())
+        return(lookup %>% transmute_all(as.character) %>% as_tibble())
 }
 
 
 #' Write value map to a file
 #'
-#' @param cover a value map
+#' @param lookup a value map
 #' @param path a character string
 #' @param overwrite overwriting existing file from \code{path}
-#' @param edit invoke \code{edit_cover} before writing to file
+#' @param edit invoke \code{edit_lookup} before writing to file
 #' @import dplyr readr
 #' @export
-write_cover <- function(cover, path, overwrite = FALSE, edit = FALSE) {
+write_lookup <- function(lookup, path, overwrite = FALSE, edit = FALSE) {
         its_there <- file.exists(path)
 
-        if (its_there & overwrite == FALSE) { # join file with new cover
-                cover0 <- read_cover(path)
-                cover <- cover %>%
+        if (its_there & overwrite == FALSE) { # join file with new lookup
+                lookup0 <- read_lookup(path)
+                lookup <- lookup %>%
                         select(-new_value) %>%
-                        left_join(cover0, by = c("col_name", "old_value"))
+                        left_join(lookup0, by = c("col_name", "old_value"))
         }
 
         if (edit) {
-                edit_cover(cover)
+                edit_lookup(lookup)
         }
 
 
-        write_csv(cover, path, na = "", append = FALSE)
+        write_csv(lookup, path, na = "", append = FALSE)
         cat("Written at ", path)
 }
 
@@ -64,65 +64,65 @@ write_cover <- function(cover, path, overwrite = FALSE, edit = FALSE) {
 #' @param path a path pointing to a value map
 #' @import readr
 #' @export
-read_cover <- function(path){suppressMessages({
+read_lookup <- function(path){suppressMessages({
         read_csv(path, col_names = TRUE, col_types = cols(.default = "c"))
 })}
 
 
 #' Edit value map
 #'
-#' @param cover a value map, or a path pointing to a file containing such
+#' @param lookup a value map, or a path pointing to a file containing such
 #' @param quiet commit the modification without confirmation?
 #' @import dplyr readr utils
 #' @export
-edit_cover <- function(cover, quiet = FALSE) {
+edit_lookup <- function(lookup, quiet = FALSE) {
 
-        if (is.character(cover)) { # read from file, if so deviced
-                path <- cover
-                cover <- read_cover(path)
+        if (is.character(lookup)) { # read from file, if so deviced
+                path <- lookup
+                lookup <- read_lookup(path)
                 message(cat("Editing a file...\n"))
         }
 
-        edited_cover <- edit(cover)
-        made_changes <- !isTRUE(all_equal(cover, edited_cover))
+        edited_lookup <- edit(lookup)
+        made_changes <- !isTRUE(all_equal(lookup, edited_lookup))
 
         if (made_changes) { # accept or discard changes
                 if (quiet) {
-                        cover <- edited_cover
+                        lookup <- edited_lookup
                 } else if (!quiet) {
                         user_selection <- menu(c("Accept", "Discard"), title = "You've made changes:")
                         if (user_selection == 1) {
-                                cover <- edited_cover
+                                lookup <- edited_lookup
                         } else {
                                 made_changes <- FALSE
                         }
                 }
         }
 
-        cover <- as_tibble(cover)
+        lookup <- as_tibble(lookup)
 
         if (exists("path")) { # write to file, if so deviced
                 if (made_changes) {
-                        write_csv(cover, path, na = "", append = FALSE)
+                        write_csv(lookup, path, na = "", append = FALSE)
                         message(cat("\nChanges saved at ", path, "\n"))
                 }
         }
 
-        invisible(cover)
+        invisible(lookup)
 }
 
 #' Use value map to replace values in a data frame
 #'
 #' @param .data a data frame
-#' @param cover a value map, or a path pointing to a file containing such
+#' @param lookup a value map, or a path pointing to a file containing such
 #' @param mark out of old and new, mark which column name? choose from: c("old", "new", "both")
 #' @param drop_old keep only the new column after recoding
 #' @import dplyr
 #' @export
-use_cover <- function(.data, cover, mark = c("old", "new", "both"), drop_old = TRUE) {
-        if (is.character(cover)) { # read from file, if so deviced
-                path <- cover
-                cover <- read_cover(path)
+use_lookup <- function(.data, lookup, mark = c("old", "new", "both"), drop_old = TRUE) {
+        if (is.character(lookup)) { # read from file, if so deviced
+                path <- lookup
+                lookup <- read_lookup(path)
                 cat("Using a file...\n")
         }
 
@@ -134,20 +134,20 @@ use_cover <- function(.data, cover, mark = c("old", "new", "both"), drop_old = T
                 drop_old <- FALSE
         }
 
-        var_names <- unique(cover$col_name)
+        var_names <- unique(lookup$col_name)
 
         res <- .data %>% mutate_at(vars(var_names), .funs = as.character) # coerce target vars to character
 
         for (var_name in var_names) {  # loop over variables, join new coding to data
-                sub_cover <- cover[cover$col_name == var_name, c(2,3)]
+                sub_lookup <- lookup[lookup$col_name == var_name, c(2,3)]
 
                 var_name_old <- paste0(var_name, "_old_")
                 var_name_new <- paste0(var_name, "_new_")
 
-                names(sub_cover) <- c(var_name,
+                names(sub_lookup) <- c(var_name,
                                       var_name_new)
 
-                res <- res %>% left_join(sub_cover, by = var_name)
+                res <- res %>% left_join(sub_lookup, by = var_name)
 
                 if (mark == "both") {
                         names(res)[names(res) == var_name] <- var_name_old
@@ -169,35 +169,35 @@ use_cover <- function(.data, cover, mark = c("old", "new", "both"), drop_old = T
 
 #' Batch modify a value map
 #'
-#' @param cover a value map
+#' @param lookup a value map
 #' @param .fun a function to be applied to old_value
 #' @param ... a vector containing col_name
 #' @import dplyr readr
 #' @export
-modify_cover <- function(cover, .fun, ...){
-        if (is.character(cover)) { # read from file, if so deviced
-                path <- cover
-                cover <- read_cover(path)
+modify_lookup <- function(lookup, .fun, ...){
+        if (is.character(lookup)) { # read from file, if so deviced
+                path <- lookup
+                lookup <- read_lookup(path)
                 message(cat("Modifying a file...\n"))
         }
 
         dots <- paste(match.call(expand.dots = FALSE)$...)
-        if (length(dots) == 0) dots <- unique(cover$col_name)
+        if (length(dots) == 0) dots <- unique(lookup$col_name)
 
         fun <- as_mapper(.fun)
 
-        sub_cover <- cover %>%
+        sub_lookup <- lookup %>%
                 filter(col_name %in% dots) %>%
                 mutate(new_value = fun(old_value))
 
-        cover <- cover %>%
-                anti_join(sub_cover, by = c("col_name", "old_value")) %>%
-                bind_rows(sub_cover)
+        lookup <- lookup %>%
+                anti_join(sub_lookup, by = c("col_name", "old_value")) %>%
+                bind_rows(sub_lookup)
 
 
         if (exists("path")) { # write to file, if so deviced
-                write_csv(cover, path, na = "", append = FALSE)
+                write_csv(lookup, path, na = "", append = FALSE)
         }
-        invisible(cover)
+        invisible(lookup)
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# jollyrogr
+# rlookup
